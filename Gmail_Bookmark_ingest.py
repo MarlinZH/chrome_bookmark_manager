bb5d306d08c7c83bb327c58792d50c7da100c5b8
Marlin <marlin.z.hicks@gmail.com>
2024-07-01 02:05:02 -0400

    Class optimization in analyze update folder detection

--- Gmail_Bookmark_ingest.py
+++ Gmail_Bookmark_ingest.py
@@ -2,43 +2,66 @@ import spacy
 import json
 import pandas as pd
 
-
 def load_bookmarks(bookmarks_path):
     with open(bookmarks_path,"r",encoding='utf-8') as file:
         bookmarks_content = json.load(file)
         return bookmarks_content
+    
+#READ ROOT LEVEL OF FILE
+def identify_folders(bookmarks_path):
+    bookmarks_content = load_bookmarks(bookmarks_path)
+    print(bookmarks_content)
+    bookmarks_root = bookmarks_content['roots']
+    bookmark_bar = bookmarks_root['bookmark_bar']
+    bookmark_bar_children = bookmark_bar['children']
+    for child in bookmark_bar_children:
+        if child['type'] == 'folder' :
+            print(child['name'])
 
-def analyze_bookmarks(bookmark_content):
-    bookmarks_list = bookmark_content['roots']
-    # print(bookmarks_list)
-    bookmark_bar = bookmarks_list['bookmark_bar']
-    print(bookmark_bar)
-    bookmark_folders = bookmark_bar['children']
-    print(bookmark_folders)
-    for bookmark in bookmark_folders:
-        print(bookmark['name'])
-        # nlp = spacy.load("en_core_web_sm")
-        # for bookmark in bookmark_list['roots']['bookmark_bar']['children']:
-        #     title = bookmark_list.get('name', '')
-        #     # print(title)
-        #     doc = nlp(title)
-        #     print(doc)
-    #     for book in bookmark[0]:
-    #         print(book)
-    return bookmark_bar,bookmarks_list,bookmark
+    print("ROOT:",bookmarks_root)
+    bookmarks_folder = []
+    for bookmark in bookmarks_content:
+        if bookmark["type"] == "folder":
+            print(bookmark)
 
 
-def organize_bookmarks(bookmark_list):
-    bookmarks_dict = {}
+ 
 
+    # bookmark_bar = bookmarks_list['bookmark_bar']
+    # print(bookmark_bar)
+    # bookmark_folders = bookmark_bar['children']
+    # print(bookmark_folders)
+
+
+# def analyze_bookmarks(bookmark_content):
+#     for bookmark in bookmark_folders:
+#         print(bookmark['name'])
+#         # nlp = spacy.load("en_core_web_sm")
+#         # for bookmark in bookmark_list['roots']['bookmark_bar']['children']:
+#         #     title = bookmark_list.get('name', '')
+#         #     # print(title)
+#         #     doc = nlp(title)
+#         #     print(doc)
+#     #     for book in bookmark[0]:
+#     #         print(book)
+#     return bookmark_bar,bookmarks_list,bookmark
+
+
+# def organize_bookmarks(bookmark_list):
+#     bookmarks_dict = {}
 
 
 def main():
-    bookmarks_path = "C:\\Users\\FROAP\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Bookmarks"
-    bookmarks = load_bookmarks(bookmarks_path)
-    analyzer = analyze_bookmarks(bookmarks)
-    organizer = organize_bookmarks(analyzer)
-    print(organizer)
+    bookmarks_path = r"C:\Users\FROAP\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+    # bookmarks_content = load_bookmarks(bookmarks_path)
+    # print(bookmark_file)
+    bookmarks_root = identify_folders(bookmarks_path)
+    # print(bookmarks_root)
+    # folders = identify_folders(bookmarks_content)
+    # print(folders)
+    # analyzer = analyze_bookmarks(bookmarks)
+    # organizer = organize_bookmarks(analyzer)
+    # print(organizer)
     # print(json.dumps(bookmarks, indent=1))
 if __name__ == "__main__":
      main()
