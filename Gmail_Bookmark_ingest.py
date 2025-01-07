08ca02fb57edba8746f71111cb0480a131ac3ec6
Marlin <marlin.z.hicks@gmail.com>
2025-01-06 23:30:56 -0500

    Updated Dataframe

--- Gmail_Bookmark_ingest.py
+++ Gmail_Bookmark_ingest.py
@@ -1,77 +1,40 @@
-import spacy
 import json
 import pandas as pd
 
 def load_bookmarks(bookmarks_path):
-    with open(bookmarks_path,"r",encoding='utf-8') as file:
-        bookmarks_content = json.load(file)
+    try:
+        with open(bookmarks_path, "r", encoding="utf-8") as file:
+            bookmarks_content = json.load(file)
         return bookmarks_content
-    
-#READ ROOT LEVEL OF FILE
+    except FileNotFoundError:
+        print(f"Error: File not found at {bookmarks_path}")
+        return None
+    except json.JSONDecodeError:
+        print("Error: File is not a valid JSON.")
+        return None
+
+def extract_folders(children, bookmarks_folders):
+    for child in children:
+        if child['type'] == 'folder':
+            bookmarks_folders.append(child['name'])
+            if 'children' in child:
+                extract_folders(child['children'], bookmarks_folders)
+
 def identify_folders(bookmarks_path):
     bookmarks_content = load_bookmarks(bookmarks_path)
-    # print(bookmarks_content)
+    if not bookmarks_content:
+        return
     bookmarks_root = bookmarks_content['roots']
     bookmark_bar = bookmarks_root['bookmark_bar']
     bookmark_bar_children = bookmark_bar['children']
     bookmarks_folders = []
-    for child in bookmark_bar_children:
-        if child['type'] == 'folder' :
-            bookmark_folder = child['name']
-            bookmarks_folders.append(bookmark_folder)
-            bookmark_folder_id = child['guid']
-            bookmark_last_used = child['date_last_used']
-            for children in child['children']:
-                  if children['type'] == 'folder':
-                   nested_children = children['name']
-                   bookmarks_folders.append(nested_children)
-    df = pd.DataFrame(bookmarks_folders, columns='Folders')
-    # print('Folders',bookmarks_folders)
-    print(df )
-    # return bookmarks_folders
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-# def analyze_bookmarks(bookmark_content):
-#     for bookmark in bookmark_folders:
-#         print(bookmark['name'])
-#         # nlp = spacy.load("en_core_web_sm")
-#         # for bookmark in bookmark_list['roots']['bookmark_bar']['children']:
-#         #     title = bookmark_list.get('name', '')
-#         #     # print(title)
-#         #     doc = nlp(title)
-#         #     print(doc)
-#     #     for book in bookmark[0]:
-#     #         print(book)
-#     return bookmark_bar,bookmarks_list,bookmark
-
-
-# def organize_bookmarks(bookmark_list):
-#     bookmarks_dict = {}
-
+    extract_folders(bookmark_bar_children, bookmarks_folders)
+    df = pd.DataFrame(bookmarks_folders, columns=['Folders'])
+    print(df)
 
 def main():
     bookmarks_path = r"C:\Users\FROAP\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
-    # bookmarks_content = load_bookmarks(bookmarks_path)
-    # print(bookmark_file)
-    bookmarks_root = identify_folders(bookmarks_path)
-    # print(bookmarks_root)
-    # folders = identify_folders(bookmarks_content)
-    # print(folders)
-    # analyzer = analyze_bookmarks(bookmarks)
-    # organizer = organize_bookmarks(analyzer)
-    # print(organizer)
-    # print(json.dumps(bookmarks, indent=1))
+    identify_folders(bookmarks_path)
+
 if __name__ == "__main__":
-     main()
+    main()
